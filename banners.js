156b79ebb4f5f343850a7506d329e3a860b0c55f
dharmpallodhi2-rgb <dharmpallodhi2@gmail.com>
2026-08-06 19:18:41 +0530

    Update banners.js

--- banners.js
+++ banners.js
@@ -1,13 +1,7 @@
 const BANNERS_DATA = [
     {
         id: 1,
-        imageUrl: "https://placehold.co/800x300/ffedd5/f97316?text=सुनो,+समझो+और+सफलता+की+ओर+बढ़ो!",
-        link: "#",
-        isActive: true
-    },
-    {
-        id: 2,
-        imageUrl: "coming_soon.png", // आपकी कमिंग सून वाली फोटो
+        imageUrl: "File_00000000ab1c81fa8bbaaded2eaed436.png",
         link: "#",
         isActive: true
     }
