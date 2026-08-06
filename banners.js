fb08d0d696f7ca76deaa7e07cca0d20dffbedbc9
dharmpallodhi2-rgb <dharmpallodhi2@gmail.com>
2026-08-06 14:42:51 +0530

    Create banners.js

---
+++ banners.js
@@ -0,0 +1,14 @@
+const BANNERS_DATA = [
+    {
+        id: 1,
+        imageUrl: "https://placehold.co/800x300/ffedd5/f97316?text=सुनो,+समझो+और+सफलता+की+ओर+बढ़ो!",
+        link: "#",
+        isActive: true
+    },
+    {
+        id: 2,
+        imageUrl: "coming_soon.png", // आपकी कमिंग सून वाली फोटो
+        link: "#",
+        isActive: true
+    }
+];
